0bf7a152c4658fe64590bcc43100f070f1869292
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2025-01-03 10:40:05 +0100

    first date with ganache cli

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
     }
   },
+  compilers: {
+    solc: {
+      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
 };
